ad3a4768f7a1d9a9a14084fe27704ecd5469edae
8bitty <max@koeninger.org>
2023-01-14 17:45:05 -0600

    added name system

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+import random
+import sys
+import time
+
+#checks whether you have a name, and if not, prompts you to input one
+a=open("info.txt","a")
+a.close()
+with open("info.txt","r") as f:
+    global named
+    if(f.readline()==""):
+        named=False
+    else:
+        named=True
+if(named==False):
+    with open("info.txt","w") as f:
+        name=input("please input a name: ")
+        f.write(name)
+else:
+    pass
